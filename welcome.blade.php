65fc12bca9b9bdae55a59681882026c5d32614c1
wannurina <47972821+wannurina@users.noreply.github.com>
2019-12-01 00:59:25 +0800

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,165 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Merged Forms Page Responsive Widget Template :: W3layouts</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords"
+        content="Merged Forms Page Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <!-- //Meta tag Keywords -->
+     <link rel="stylesheet" href="css/homepage.css" type="text/css" media="all" /><!-- Style-CSS -->
+</head>
+<body>
+   <!-- /form-26-section -->
+   <section class="form-26">
+         <div class="form-26-mian">
+				<div class="layer">
+			<div class="wrapper">
+			<div class="form-inner-cont">
+					<div class="forms-26-info">
+						 <h2>IIUM Vehicle Registration System</h2>
+                        <p>Apply Your Sticker Now!</p>
+                    </div>
+					<div class="form-right-inf">
+            <form action="login">
+                @csrf
+							 <div class="forms-gds">
+                 <div class="form-input editContent" data-selector=".editContent" style="outline: none; cursor: inherit;">
+                   <button type="submit" class="btn btn-primary" data-selector="a.btn, button.btn" style="outline: none; cursor: inherit;">
+                     <a href="{{ route('login') }}">Login as Student</a></button>
+
+                 </div></form>
+                 <form action="loginStaff">
+                     @csrf
+                 <div class="form-input editContent" data-selector=".editContent" style="outline: none; cursor: inherit;">
+                   <button class="btn" data-selector="a.btn, button.btn" style="outline: none; cursor: inherit;">
+                     <a href="{{ url('loginStaff') }}">Login as Staff</a></button>
+                 </div>
+
+							</div>
+
+						</form>
+
+                    </div>
+				<div class="copyright text-center">
+                    <p>© 2019 All rights reserved | Designed by Zu and Ina</p>
+                </div>
+                </div>
+
+			</div>
+		</div>
+		    </div>
+		</section>
+      <!-- //form-26-section -->
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="{{url ('form')}}">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
